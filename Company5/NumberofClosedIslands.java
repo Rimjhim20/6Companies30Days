754fbca294f31262e3274e80d89a7246d5d1a6b0
Rimjhim20 <kumaririmjhim720@gmail.com>
2023-01-23 21:48:37 +0530

    day23

--- Company5/NumberofClosedIslands.java
+++ Company5/NumberofClosedIslands.java
@@ -40,7 +40,7 @@ class Solution {
     static void dfs(int[][] grid, int i, int j) {
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 1 || grid[i][j] == 2)
             return;
-        grid[i][j] = 2;
+        grid[i][j] = 2 ;
         dfs(grid, i - 1, j);
         dfs(grid, i, j - 1);
         dfs(grid, i + 1, j);
